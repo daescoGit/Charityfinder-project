501bf43c5a5603b38eded6e89f19624bdf9e9f9e
dan <daneskildsen1234@gmail.com>
2020-06-03 17:35:27 +0200

    cleaning serializers, api-view, permissions

--- charityfinder_app/api.py
+++ charityfinder_app/api.py
@@ -3,7 +3,8 @@ from rest_framework.decorators import api_view, permission_classes
 from rest_framework.response import Response
 from comment_app .models import Comment
 from .serializers import CommentSerializer, UserSerializer
-from .permissions import IsAuthorOrReadOnly, IsOwnerOrReadOnly, AllowRead, IsLoggedOrReadOnly
+from .permissions import IsAuthorOrReadOnly, IsOwnerOrAdmin
+from rest_framework.permissions import AllowAny, IsAuthenticatedOrReadOnly
 from django.contrib.auth import get_user_model
 from django.core.cache import cache
 from .gg_api_call import gg_call_project, gg_call_project_list
@@ -12,8 +13,7 @@ from .gg_api_call import gg_call_project, gg_call_project_list
 class CommentList(generics.ListCreateAPIView):
     queryset = Comment.objects.all()
     serializer_class = CommentSerializer
-    permission_classes = (IsLoggedOrReadOnly,)
-    # todo add author permission/restriction to creating comment (list view permission)
+    permission_classes = (IsAuthenticatedOrReadOnly,)
 
 
 class CommentDetail(generics.RetrieveUpdateAPIView):
@@ -25,19 +25,18 @@ class CommentDetail(generics.RetrieveUpdateAPIView):
 class UserList(generics.ListAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-    permission_classes = (AllowRead,)
-    # todo change to admin only view
+    # base permission = admin only
 
 
 class UserDetail(generics.RetrieveUpdateDestroyAPIView):
     queryset = get_user_model().objects.all()
     serializer_class = UserSerializer
-    permission_classes = (IsOwnerOrReadOnly,)
+    permission_classes = (IsOwnerOrAdmin,)
 
 
 # function/decorator based api view
 @api_view(['GET'])
-@permission_classes((AllowRead,))
+@permission_classes((AllowAny,))
 def project_detail(request, pid):
     query = 'https://api.globalgiving.org/api/public/projectservice/projects/'
     gg_call_project(pid, query)
@@ -45,7 +44,7 @@ def project_detail(request, pid):
 
 
 @api_view(['GET'])
-@permission_classes((AllowRead,))
+@permission_classes((AllowAny,))
 def project_list(request):
     query = 'https://api.globalgiving.org/api/public/projectservice/all/projects/active/ids'
     gg_call_project_list(query)
